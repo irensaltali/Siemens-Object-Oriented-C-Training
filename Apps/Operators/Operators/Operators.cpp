be3f3285ee881826792ac2bd9cf551b64b34b598
İren SALTALI <iren@saltali.com>
2019-01-24 10:58:17 +0300

    Operators update

--- Apps/Operators/Operators/Operators.cpp
+++ Apps/Operators/Operators/Operators.cpp
@@ -1,4 +1,4 @@
-// Operators.cpp : This file contains the 'main' function. Program execution begins and ends there.
+﻿// Operators.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include "pch.h"
@@ -36,10 +36,51 @@ void logicalANDwithIf() {
 	}
 }
 
+void ternaryConditional() {
+	int a;
+	std::cout << "Bir sayi giriniz:";
+	std::cin >> a;
+	std::cout << (a % 2 == 0 ? "ciftir" : "tektir");
+}
+
+void sizeOfFunction() {
+	int a;
+	std::cout << "Bir sayi giriniz:";
+	std::cin >> a;
+	std::cout << "int boyutu: " << sizeof(a) << "\n";
+
+
+	char b;
+	std::cout << "Bir harf giriniz:";
+	std::cin >> b;
+	std::cout << "char boyutu:" << sizeof(b) << "\n";
+	std::cout << "b: " << b << "\n";
+
+
+	char c[100];
+	std::cout << "Bir metin giriniz:";
+	std::cin >> c;
+	std::cout << "char array boyutu:" << sizeof(c) << "\n";
+	std::cout << "c: " << c << "\n";
+
+}
+
+void orderOfOperations() {
+	int a = 4;
+	int b = 32;
+	int c = 3232;
+	int d = 11;
+	int g = 1;
+	std::cout << (a + b * c / d % -g);
+}
+
 int main()
 {
 	arithmeticLeftShift();
 	bitwiseANDAssignment();
 	logicalAND();
 	logicalANDwithIf();
+	ternaryConditional();
+	sizeOfFunction();
+	orderOfOperations();
 }
