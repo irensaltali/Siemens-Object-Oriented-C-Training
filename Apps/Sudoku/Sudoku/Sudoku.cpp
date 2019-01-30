22fc3b63f2252a44cae4399da4007811aa3fcefd
İren SALTALI <iren@saltali.com>
2019-01-30 15:12:10 +0300

    2

--- Apps/Sudoku/Sudoku/Sudoku.cpp
+++ Apps/Sudoku/Sudoku/Sudoku.cpp
@@ -1,4 +1,4 @@
-// Sudoku.cpp : This file contains the 'main' function. Program execution begins and ends there.
+﻿// Sudoku.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include "pch.h"
@@ -19,13 +19,13 @@ public:
 					cout << "\t";
 			}
 			cout << endl;
-			if((i+1)%3==0)
+			if ((i + 1) % 3 == 0)
 				cout << endl;
 
 		}
 	}
 
-	bool satirKontrol(int sayi,int satir) {
+	bool satirKontrol(int sayi, int satir) {
 		bool sonuc = false;
 		for (int i = 0; i < 9; i++) {
 			if (matrix[satir][i] == sayi) {
@@ -35,6 +35,7 @@ public:
 		}
 		return sonuc;
 	}
+	
 	bool sutunKontrol(int sayi, int sutun) {
 		bool sonuc = false;
 		for (int i = 0; i < 9; i++) {
@@ -46,12 +47,36 @@ public:
 		return sonuc;
 	}
 
+	bool kutuKontrol(int sayi, int satir, int sutun) {
+		int baslangicSatir = satir / 3;
+		baslangicSatir *= 3;
+		int baslangicSutun = sutun / 3;
+		baslangicSutun *= 3;
+		bool sonuc = false;
+
+		for (int i = baslangicSatir; i < baslangicSatir + 3; i++) {
+			for (int j = baslangicSutun; j < baslangicSutun + 3; j++) {
+				if (matrix[i][j] == sayi) {
+					sonuc = true;
+					break;
+				}
+			}
+		}
+		return sonuc;
+	}
+
+	void sayiGir(int sayi, int satir, int sutun) {
+		if (!satirKontrol(sayi, satir) && !sutunKontrol(sayi, sutun) && !kutuKontrol(sayi, satir, sutun)) {
+			matrix[satir][sutun] = sayi;
+			yazdir();
+		}
+	}
 private:
 	int matrix[9][9] = {
 		{9,0,0,5,0,0,0,0,0},
 		{6,8,7,3,4,0,0,5,0},
 		{0,0,4,1,0,7,0,3,0},
-			
+
 		{0,4,0,0,2,6,0,0,8},
 		{0,2,6,0,5,1,4,9,0},
 		{0,9,1,4,0,3,0,6,2},
@@ -67,6 +92,33 @@ int main()
 	cout << "Sudoku oyunumuza hos geldiniz\n\n";
 	Sudoku sudoku;
 
-	cout << sudoku.satirKontrol(7, 2);
-	cout << sudoku.sutunKontrol(5, 4);
+	/*cout << sudoku.satirKontrol(7, 2) << endl;
+	cout << sudoku.sutunKontrol(5, 4) << endl;
+	cout << sudoku.kutuKontrol(1, 0, 0) << endl;*/
+	int x = 0;
+	do {
+
+		cout << "Oyunu bitirmek icin -1 giriniz." << endl;
+		cout << "Yazdirmak icin 1 giriniz." << endl;
+		cout << "Sayi girmek icin 2'ye basin. " << endl;
+		cin >> x;
+
+
+		if (x == 1)
+			sudoku.yazdir();
+		if (x == 2) {
+			int sayi, satir, sutun;
+			cout << "Girmek istedigiz sayi:";
+			cin >> sayi;
+
+			cout << "Girmek istediginiz satir:";
+			cin >> satir;
+
+			cout << "Girmek istediginiz sutun:";
+			cin >> sutun;
+
+			sudoku.sayiGir(sayi, satir-1, sutun-1);
+		}
+		
+	} while (x>0);
 }
